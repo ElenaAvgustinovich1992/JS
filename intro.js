d6586063828ea9f28fb8cbe99be499bd37ab98d0
Elena Avgustinovich1992 <lena.leno430@gmail.com>
2023-07-26 16:22:20 +0300

    HW_homework js_1

--- intro.js
+++ intro.js
@@ -1,80 +1,150 @@
+HW_1
+
+1. Create a variable "item_1":
 
 var item_1;
 
+2. Assign the value 5 to the variable item_1:
+
 item_1=5;
+
+3. Print item_1 to the console:
 
 console.log(item_1);
 
+4. Create a variable "item_2":
+
 var item_2;
+
+5. Assign the value 3 to the variable item_2:
 
 item_2=3;
 
+6. Print item_2 to the console:
+
 console.log(item_2);
+
+7. Create a variable "item_3":
 
 var item_3;
 
+8. Assign the sum of item_1 and item_2 to the variable item_3:
+
 item_3=item_1+item_2;
+
+9. Print item_3 to the console:
 
 console.log(item_3);
 
+10. Create a variable "item_4":
+
 var item_4;
+
+11. Assign the string "Yolochka" to the variable item_4:
 
 item_4="Yolochka";
 
+12. Print item_4 to the console:
+
 console.log(item_4);
+
+13. Print the sum of item_3 and item_4 to the console:
 
 console.log(item_3+item_4);
 
+14. Print to the console the multiplication of item_3 and item_4:
+
 console.log(item_3*item_4);
+
+15. Create a variable "item_5":
 
 var item_5;
 
+16. Assign the value of item_3 to the variable item_5:
+
 item_5=item_3;
+
+17. Create a variable "item_6":
 
 var item_6;
 
+18. Create a variable "item_6_type":
+
 var item_6_type;
+
+19. Assign the value 15 to the variable item_6:
 
 item_6=15;
 
+20. Assign the type of variable item_6 to the variable item_6_type:
+
 item_6_type=item_6;
+
+21. Print the string type of item_6 in the format item_6 == item_6,
+ item_6_type == item_6_type to the console:
 
 console.log("item_6" , typeof item_6.toString(), item_6==item_6 && 
 "item_6_type" , typeof item_6_type.toString(), item_6_type==item_6_type );
 
+22. Create a variable "item_7" and convert item_6 to a string:
+
 const item_7=item_6.toString();
+
+23. Create a variable "item_7_type":
 
 var item_7_type;
 
+24. Assign the type of variable item_7 to the variable item_7_type:
+
 item_7_type=item_7;
+
+25. Print the string type of item_7 in the format item_7 == item_7,
+ item_7_type == [data type]" to the console:
+ item_7_type ==   to the console:
 
 console.log("item_7" , typeof item_7, item_7==item_7  &&
 "item_7_type",typeof item_7_type, item_7_type==item_7_type);
 
+26. Create a variable "age_1" and assign it the value 10:
+
 var age_1=10;
+
+27. Create a variable "age_2" and assign it the value 18:
 
 var age_2=18;
 
+28. Create a variable "age_3" and assign it the value 60:
+
 var age_3=60;
+
+29. Create an if statement to check the value of variable age_1:
 
 if (age_1>=10) {
 console.log ("True")
-}
-else  {
-console.log ("False")
 };
+
+30. If age_1 is less than age_2, print "You don’t have access cause your age is [age_1].
+ It’s less than [age_2]." to the console:
 
 if (age_1<age_2){
 console.log("You don’t have access cause your age is " , age_1 , " It’s less then ")
 };
 
+31. If age_1 is greater than or equal to age_2 and less than age_3, print "Welcome!" 
+to the console:
+
 if (age_1>=age_2 && age_1<age_3){
     console.log("Welcom!")
 };
 
+32. If age_1 is greater than age_3, print "Keep calm and look Culture channel" 
+to the console:
+
 if (age_1>age_3){
     console.log("Keep calm and look Culture channel")
 };
+
+33. Otherwise, print "Technical work" to the console:
 
 if (age_1>age_3){
     console.log("Keep calm and look Culture channel")
@@ -82,10 +152,6 @@ if (age_1>age_3){
 else {
 console.log("Technical work")
 };
-
-
-
-
 
 
 
